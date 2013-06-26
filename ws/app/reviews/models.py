ce6f5d63d7ea8b7e71ee34b5515bcfbbd014e5cf
mjjc <mjjc1337@gmail.com>
2013-06-26 19:23:25 +0200

    models, review rating

--- ws/app/reviews/models.py
+++ ws/app/reviews/models.py
@@ -1,18 +1,33 @@
 from app import db
+from app.votes.models import Vote
+from utils import compute_rating
+
 from sqlalchemy.dialects.postgresql import UUID
+import datetime
 
 class Review(db.Model):
 
     __tablename__ = 'review'
-    id = db.Column(UUID, primary_key=True, 
-                   server_default=db.text("uuid_generate_v4()"))
-    release_group_id = db.Column(UUID, index=True)
-    user = db.Column(UUID, db.ForeignKey('user.id'), nullable=False)
+    
+    id = db.Column(UUID(as_uuid=True), server_default=db.text('uuid_generate_v4()'), primary_key=True)
+    release_group_id = db.Column(UUID(as_uuid=True), index=True)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
     text = db.Column(db.String, nullable=False)
-    created = db.Column(db.DateTime, nullable=False,
-                        server_default=db.text("now()"))
-    last_updated = db.Column(db.DateTime, server_default=db.text("null"))
-    edits = db.Column(db.Integer, nullable=False,
-                      server_default=db.text("0"))
-    votes = db.relationship("Vote")
-    reports = db.relationship("SpamReport")
+    created = db.Column(db.DateTime, nullable=False, server_default=db.text("now()"))
+    last_updated = db.Column(db.DateTime, onupdate=datetime.datetime.now)
+    edits = db.Column(db.Integer, nullable=False, server_default=db.text("0"))
+    rating = db.Column(db.Integer, nullable=False, server_default=db.text("0"))
+    
+    user = db.relationship('User', backref=db.backref('reviews', lazy='dynamic'))
+
+    def __init__(self, user, text, release_group_id):
+        self.user = user
+        self.text = text
+        self.release_group_id = release_group_id
+        
+    def update_rating(self):
+        """ Compute and update the rank """
+        overall = self.votes.count()        
+        positive = self.votes.filter(Vote.type != 0).count()
+        self.rating = compute_rating(overall, positive)
+        return
--- ws/app/users/models.py
+++ ws/app/users/models.py
@@ -4,5 +4,6 @@ from sqlalchemy.dialects.postgresql import UUID
 class User(db.Model):
 
     __tablename__ = 'user'
-    id = db.Column(UUID, primary_key=True, 
-                   server_default=db.text("uuid_generate_v4()"))
+    
+    id = db.Column(UUID(as_uuid=True), primary_key=True, 
+        server_default=db.text("uuid_generate_v4()"))
